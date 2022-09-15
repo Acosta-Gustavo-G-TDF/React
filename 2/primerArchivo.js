75761db744bd347333e60ca53ea9ca5884256969
Gustavo Gaston Acosta <gustonacosta@tdf.edu.ar>
2022-09-14 23:06:15 -0300

    Actividad 06/09/22

---
+++ 2/primerArchivo.js
@@ -0,0 +1,4 @@
+console.log(require('math-random')());
+
+console.log(require('math-random/is-secure'));
+true || false;
---
+++ 2/primerArchivo.ts
@@ -0,0 +1,5 @@
+console.log(require('math-random')())
+=> 0.584293719381094
+ 
+console.log(require('math-random/is-secure'))
+=> true || false
